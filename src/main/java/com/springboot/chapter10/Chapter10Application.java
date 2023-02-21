3e4a4c0ba5ae66bfce92e4b4375d83a1746673f9
zhaohangyi <1806472041@qq.com>
2023-02-21 22:49:43 +0800

    前十章的练习代码

--- src/main/java/com/springboot/chapter10/Chapter10Application.java
+++ src/main/java/com/springboot/chapter10/Chapter10Application.java
@@ -1,9 +1,13 @@
 package com.springboot.chapter10;
 
+import com.springboot.chapter10.interceptor.Interceptor1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 //定义Spring Boot扫描包的路径
@@ -24,11 +28,19 @@ import org.springframework.stereotype.Repository;
         //markerInterface = Class.class 限定扫描接口
         annotationClass = Repository.class
 )
-public class Chapter10Application {
+public class Chapter10Application implements WebMvcConfigurer {
 
     //启动切面
     public static void main(String[] args) {
         SpringApplication.run(Chapter10Application.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册拦截器到Spring MVC机制，然后它会返回一个拦截器注册
+        InterceptorRegistration ir = registry.addInterceptor(new Interceptor1());
+        //指定拦截匹配模式，限制拦截器拦截请求
+        ir.addPathPatterns("/interceptor/*");
     }
 
 }
